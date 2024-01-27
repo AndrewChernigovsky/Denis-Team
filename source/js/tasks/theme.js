13d23584479371ffb17fc3777eb9182e6aeb568f
Andrew Chernigovsky <chernigovsky108@gmail.com>
2024-01-27 22:23:54 +0300

    theme

--- source/js/tasks/theme.js
+++ source/js/tasks/theme.js
@@ -2,9 +2,15 @@ const BODY = document.querySelector('body');
 const TOGGLE_THEME = document.getElementById('themeToggle');
 const ACTIVE = 'theme-active';
 
+const elementsWithDataTheme = document.querySelectorAll('[data-theme]');
+
+// Перебираем найденные элементы
 
 export function theme() {
 	TOGGLE_THEME.addEventListener('click', () => {
 		BODY.classList.toggle(ACTIVE);
+		elementsWithDataTheme.forEach(function (element) {
+			element.classList.toggle(ACTIVE);
+		});
 	})
 }
