9e7e6163b01f297c65cb494cc5cfb1288e6270fe
Andrew Chernigovsky <chernigovsky108@gmail.com>
2024-01-27 22:35:03 +0300

    fixes

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,27 @@ import startServer from './gulp-tasks/server.js';
 import copyAssets from './gulp-tasks/copyAssets.js';
 import config from './gulp-config.js';
 
+import ghpages from 'gh-pages';
+
+const ghpages1 = ghpages;
+
+export function publishGithubPages(cb) {
+	ghpages1.publish('build', {
+		branch: 'gh-pages',
+		repo: 'https://github.com/AndrewChernigovsky/Denis-Team.git'
+	}, (err) => {
+		if (err) {
+			console.error(err);
+		} else {
+			console.log('Ваша страница успешно опубликована на GitHub Pages!');
+		}
+	});
+	cb();
+}
 export function runDev(done) {
 	config.series(deleteBuild, createBuild, config.parallel(html, styles, scripts), startServer)(done);
 }
 export function runBuild(done) {
 	config.isDev = false;
-	config.series(deleteBuild, createBuild, scripts, html, styles, copyAssets, startServer)(done);
+	config.series(deleteBuild, createBuild, config.parallel(html, styles, scripts), copyAssets, startServer, publishGithubPages)(done);
 }
