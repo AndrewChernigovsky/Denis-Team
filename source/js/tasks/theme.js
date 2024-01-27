ed2d98cb4ec02dcc269e57bf843dfc632e3bf446
Andrew Chernigovsky <chernigovsky108@gmail.com>
2024-01-27 22:00:33 +0300

    gulp js

--- source/js/tasks/theme.js
+++ source/js/tasks/theme.js
@@ -1,6 +1,5 @@
 const BODY = document.querySelector('body');
 const TOGGLE_THEME = document.getElementById('themeToggle');
-const BUTTONS_IMAGES_THEME = document.querySelectorAll(".button-icon-theme");
 const ACTIVE = 'theme-active';
 
 
@@ -8,5 +7,4 @@ export function theme() {
 	TOGGLE_THEME.addEventListener('click', () => {
 		BODY.classList.toggle(ACTIVE);
 	})
-
 }
