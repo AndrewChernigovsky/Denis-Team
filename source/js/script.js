bbe6e2c0a60fcc7401d817aae57c43d171dfa0e0
Andrew Chernigovsky <chernigovsky108@gmail.com>
2024-01-31 23:22:22 +0300

    products js-feature

--- source/js/script.js
+++ source/js/script.js
@@ -4,8 +4,12 @@ import {
 import {
 	modal
 } from "./tasks/modal.js";
+import {
+	renameAllPaths
+} from "./tasks/renameAllPaths.js";
 
 document.addEventListener('DOMContentLoaded', () => {
 	theme();
 	modal();
+	renameAllPaths();
 })
